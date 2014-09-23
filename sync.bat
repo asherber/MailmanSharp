59a1a4e313c41cbcf86d43413577ef882252caae
Aaron <Aaron@bc798c11-6c20-9a46-872d-b4c02895e90f>
2014-09-23 22:04:52 +0000

    Explicit files section in nuspec to avoid copying build.bat; add sync.bat
    git-svn-id: svn://localhost/Mailman@766 bc798c11-6c20-9a46-872d-b4c02895e90f

---
+++ sync.bat
@@ -0,0 +1,3 @@
+xcopy MailmanSharp\*.* "..\Mailman (CodePlex)\MailmanSharp" /S/Y
+xcopy MailmanSharp.Net4\*.* "..\Mailman (CodePlex)\MailmanSharp.Net4" /S/Y
+xcopy NuGet\*.* "..\Mailman (CodePlex)\NuGet" /S/Y
