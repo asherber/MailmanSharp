a928d1c41e7af2f3b76188b92e795d4c299979de
Aaron Sherber <aaron@sherber.com>
2018-01-07 21:23:41 -0500

    Get rid of NuGet directory

--- NuGet/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@set msbuild_path="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\bin"
-pushd ..
-%msbuild_path%\msbuild /v:m /p:Configuration=Release
-popd
-echo.
-nuget pack 
