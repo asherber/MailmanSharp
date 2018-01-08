06b9cc47fbb4f5b850a8b00b46c8c35924b23831
Aaron Sherber <aaron@sherber.com>
2018-01-07 20:41:31 -0500

    Add icons, update README

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -1,5 +1,5 @@
 @echo off
-@set msbuild_path="C:\Program Files (x86)\MSBuild\12.0\bin"
+@set msbuild_path="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\bin"
 pushd ..
 %msbuild_path%\msbuild /v:m /p:Configuration=Release
 popd
