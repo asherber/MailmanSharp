3c275ffbdc1295aaf4e19021c7f5aa7cabcc9d14
Aaron Sherber <aaron@sherber.com>
2018-01-14 10:15:36 -0500

    Add proper integration tests project

---
+++ MailmanSharp.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MailmanSharp.IntegrationTests
+{
+    public class IntegrationTests
+    {
+        [Fact]
+        public async void Foo()
+        {
+            var list = new MailmanList(ListConfig.Url, ListConfig.Password);
+            await list.Membership.ReadAsync();
+            list.Membership.EmailList.Should().HaveCount(2);
+        }
+    }
+}
---
+++ MailmanSharp.IntegrationTests/ListConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailmanSharp.IntegrationTests
+{
+    public class ListConfig
+    {
+        public static string Url => ConfigurationManager.AppSettings["Url"];
+        public static string Password => ConfigurationManager.AppSettings["Password"];
+    }
+}
